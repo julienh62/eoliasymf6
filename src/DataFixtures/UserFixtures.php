c9fac6f5a14f32085c861c5e3e2ce84472658313
julienh62 <jhennebo@gmail.com>
2022-12-06 17:15:07 +0100

    second commit

---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Faker;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\String\Slugger\SluggerInterface;
+
+class UserFixtures extends Fixture
+{
+    public function __construct(
+        private UserPasswordHasherInterface $passwordEncoder,
+        private SluggerInterface $slugger
+    )
+    {
+
+    }
+
+    public function load(ObjectManager $manager): void
+    {
+        $admin = new User();
+        $admin->setEmail('jhennebo@gmail.com');
+        $admin->setLastname('hennebo');
+        $admin->setFirstname('julien');
+        $admin->setAddress('6 bd liberté');
+        $admin->setCity('Berck');
+        $admin->setCountry('France');
+        $admin->setPhone('0656565656');
+        $admin->setZipcode('62600');
+        $admin->setPassword(
+            $this->passwordEncoder->hashPassword($admin, 'azerty')
+        );
+        $admin->setRoles(['ROLE_ADMIN']);
+
+        $manager->persist($admin);
+
+        $faker = Faker\Factory::create('fr_FR');
+
+        for($usr = 1; $usr <= 5; $usr++){
+            $user = new User();
+            $user->setEmail($faker->email);
+            $user->setLastname($faker->lastName);
+            $user->setFirstname($faker->firstName);
+            $user->setAddress($faker->streetAddress);
+            $user->setCity($faker->city);
+            $user->setCountry($faker->country);
+            $user->setPhone($faker->phoneNumber);
+            $user->setZipcode(str_replace(' ', '', $faker->postcode));
+            //remplacer les espaces par rien str replace
+            $user->setPassword(
+                $this->passwordEncoder->hashPassword($user, 'secret')
+            );
+
+
+            $manager->persist($user);
+
+        }
+
+
+        $manager->flush();
+    }
+}
---
+++ src/Entity/Order.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\OrderRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: OrderRepository::class)]
+#[ORM\Table(name: '`order`')]
+class Order
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\ManyToOne(inversedBy: 'orders')]
+    private ?User $article = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?int $price = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getArticle(): ?User
+    {
+        return $this->article;
+    }
+
+    public function setArticle(?User $article): self
+    {
+        $this->article = $article;
+
+        return $this;
+    }
+
+    public function getPrice(): ?int
+    {
+        return $this->price;
+    }
+
+    public function setPrice(?int $price): self
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+}
